dc85885febdc9a3579ad145a5417ae3dd9160698
Crazydoub <crazydoub@gmail.com>
2026-04-25 18:17:47 +0200

    Resync 25/04/2026 18:17:46,84

--- sync.bat
+++ sync.bat
@@ -1,14 +1,48 @@
 @echo off
 cd /d %~dp0
 
-echo Ajout des fichiers...
+echo ==========================
+echo   RESET + RESYNC GITHUB
+echo ==========================
+
+:: === CONFIG ===
+set REPO_URL=https://github.com/Crazydoub/clb-editor.git
+set BRANCH=main
+
+echo.
+echo [1] Verification repo git...
+if not exist .git (
+    echo Initialisation du repo...
+    git init
+)
+
+echo.
+echo [2] Fix safe directory...
+git config --global --add safe.directory "%cd%"
+
+echo.
+echo [3] Reset remote origin...
+git remote remove origin 2>nul
+git remote add origin %REPO_URL%
+
+echo.
+echo [4] Forcer branche main...
+git branch -M %BRANCH%
+
+echo.
+echo [5] Ajout fichiers...
 git add .
 
-echo Commit...
-git commit -m "Auto update"
+echo.
+echo [6] Commit...
+git commit -m "Resync %date% %time%" 2>nul
 
-echo Push vers GitHub...
-git push
+echo.
+echo [7] Push GitHub...
+git push -u origin %BRANCH% --force
 
-echo OK ✔
+echo.
+echo ==========================
+echo   RESYNC TERMINE ✔
+echo ==========================
 pause
